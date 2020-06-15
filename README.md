2b26912990b9e973a8e2ed31a6f4a318eb78c343
gardhr <24468810+gardhr@users.noreply.github.com>
2020-06-15 11:52:20 -0500

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![npm version](https://badge.fury.io/js/jalosi.svg)](https://badge.fury.io/js/jalosi)
 
 # Jalosi 
 
---
+++ package.js
@@ -0,0 +1,16 @@
+{
+ name : "jalosi",
+ version: "0.0.11",
+ description: "JAvascript LOading SImplified",
+ main: "jalosi.js",
+ repository: {
+  type: "git",
+  url: "git+https://github.com/gardhr/jalosi.git"
+ },
+ homepage: "https://github.com/gardhr/jalosi#readme",
+ author: "Gardhr",
+ license: "MIT",
+ bugs: {
+  url: "https://github.com/gardhr/jalosi/issues"
+ }
+}
