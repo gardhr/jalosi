b9840bff64ad6cc7e2a84a18a1abf8b94cad0832
gardhr <24468810+gardhr@users.noreply.github.com>
2020-06-29 01:16:10 -0500

    update

---
+++ .build.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+let system = require("../system.js/system.js")
+let jalosi = require("../jalosi/jalosi.js")
+let args = process.argv.slice(2)
+if(system("node test"))
+{
+ system("prettier --write *")
+ system("prettier --parser typescript --write package.jso")
+ system("jtj") 
+ system("jtj --write")
+ if(args[0] == "publish")
+  system(`gitt ${args[1]?args[1]:""}`, "npm publish")
+ for(let adx in args)
+ {
+  let what = args[adx]
+  if(what == "publish")
+  {
+   system("gitt", "npm publish")
+  }
+ }
+} 
+
+
