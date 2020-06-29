21b4cd9f01530da04934a308861c585beab15b0c
gardhr <24468810+gardhr@users.noreply.github.com>
2020-06-29 01:40:16 -0500

    update

--- .build.js
+++ .build.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 
-let system = require("../system.js/system.js")
-let jalosi = require("../jalosi/jalosi.js")
-let args = process.argv.slice(2)
-if(system("node test"))
-{
- system("prettier --write *")
- system("prettier --parser typescript --write package.jso")
- system("jtj") 
- system("jtj --write")
- if(args[0] == "publish")
-{
-  system(`gitt`)
- system("npm publish")
-} 
+let system = require("../system.js/system.js");
+let jalosi = require("../jalosi/jalosi.js");
+let args = process.argv.slice(2);
+if (system("node test")) {
+  system("prettier --write .build.js");
+  system("prettier --write *");
+  system("prettier --parser typescript --write package.jso");
+  system("jtj");
+  system("jtj --write");
+  if (args[0] == "publish") {
+    system(`gitt`);
+    system("npm publish");
+  }
 }
-
